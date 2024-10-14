3fe199607642d8dc827abe61b0751a3ba9f2a93d
Prakarsh Verma <prakarsh1248@gmail.com>
2024-10-14 21:37:47 +0530

    Created frontend with tailwind, and ShadCn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-red-300'>Hello 
+          <Button>New</Button>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
